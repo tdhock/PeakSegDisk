e9bece3b3012a06347e7acb3b46cc820755f1abd
Toby Hocking <tdhock5@gmail.com>
2018-11-20 09:04:17 -0800

    test crashes

--- tests/testthat/test-CRAN-PeakSegFPOP_disk.R
+++ tests/testthat/test-CRAN-PeakSegFPOP_disk.R
@@ -39,3 +39,27 @@ test_that("error for numeric penalty", {
   }, "pen.str must be a character string that can be converted to a non-negative numeric scalar")
 })
 
+data(Mono27ac, envir=environment())
+data.dir <- file.path(
+  tempfile(),
+  "H3K27ac-H3K4me3_TDHAM_BP",
+  "samples",
+  "Mono1_H3K27ac",
+  "S001YW_NCMLS",
+  "problems",
+  "chr11:60000-580000")
+dir.create(data.dir, recursive=TRUE, showWarnings=FALSE)
+write.table(
+  Mono27ac$coverage, file.path(data.dir, "coverage.bedGraph"),
+  col.names=FALSE, row.names=FALSE, quote=FALSE, sep="\t")
+pen.str <- "1952.6"
+lock.src <- system.file("extdata", "__db.lock.file", package="PeakSegDisk", mustWork=TRUE)
+lock.dest <- file.path(data.dir, paste0("__db.coverage.bedGraph_penalty=", pen.str, ".db"))
+file.copy(lock.src, lock.dest)
+coverage.bedGraph <- file.path(data.dir, "coverage.bedGraph")
+
+test_that("error for lock file", {
+  expect_error({
+    PeakSegFPOP_disk(coverage.bedGraph, pen.str)
+  }, lock.dest, fixed=TRUE)
+})
