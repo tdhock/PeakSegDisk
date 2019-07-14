537045f1d194b12d50eb945d4a0959b13fc9c360
Toby Dylan Hocking <tdhock5@gmail.com>
2019-07-14 09:47:18 +0200

    fix _file test

--- tests/testthat/test-CRAN-PeakSegFPOP_file.R
+++ tests/testthat/test-CRAN-PeakSegFPOP_file.R
@@ -25,9 +25,10 @@ write.table(
   sep="\t", row.names=FALSE, col.names=FALSE)
 
 test_that("character penalty works", {
-  names.list <- PeakSegFPOP_file(tmp, "10.5")
-  unlink(names.list$db)
-  seg.df <- read.table(names.list$segments)
+  pstr <- "10.5"
+  PeakSegFPOP_file(tmp, pstr)
+  segments.bed <- paste0(tmp, "_penalty=", pstr, "_segments.bed")
+  seg.df <- read.table(segments.bed)
   names(seg.df) <- c("chrom", "chromStart", "chromEnd", "status", "mean")
   expect_equal(paste(seg.df$chrom), c("chr1", "chr1", "chr1"))
   expect_equal(seg.df$chromStart, c(30, 10, 0))
@@ -36,11 +37,6 @@ test_that("character penalty works", {
   m <- mean(four$coverage[-1])
   expected.mean <- c(m, m, four$coverage[1])
   expect_equal(seg.df$mean, expected.mean, tol=1e-3)
-  loss.df <- read.table(names.list$loss)
-  names(loss.df) <- c(
-    "penalty", "segments", "peaks", "bases",
-    "mean.pen.cost", "total.loss", "equality.constraints",
-    "mean.intervals", "max.intervals")
 })
 
 test_that("error for numeric penalty", {
