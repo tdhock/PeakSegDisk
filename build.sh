ebd099aa09f8edc661ed751bc22df2d537ee39ed
Toby Dylan Hocking <tdhock5@gmail.com>
2019-07-16 20:36:22 +0200

    use lang r

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ cp -r PeakSegDisk PeakSegDisk-release
 grep -v Remotes PeakSegDisk/DESCRIPTION > PeakSegDisk-release/DESCRIPTION
 rm PeakSegDisk-release/tests/testthat/*
 cp PeakSegDisk/tests/testthat/test-CRAN*.R PeakSegDisk-release/tests/testthat
-PKG_TGZ=$(R CMD build PeakSegDisk-release|grep building|sed "s/.*\(PeakSegDisk.*.tar.gz\).*/\1/")
+R CMD build PeakSegDisk-release | tee build.out
+PKG_TGZ=$(grep building build.out|sed "s/.*\(PeakSegDisk.*.tar.gz\).*/\1/")
+echo $PKG_TGZ
 R CMD INSTALL $PKG_TGZ
 R CMD check --as-cran $PKG_TGZ
