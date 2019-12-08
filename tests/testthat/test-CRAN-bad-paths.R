b03b160a5524be2214d10baab9d997f9df0c171f
Toby Hocking <tdhock5@gmail.com>
2019-12-08 12:50:29 -0800

    fix fread.last

---
+++ tests/testthat/test-CRAN-bad-paths.R
@@ -0,0 +1,18 @@
+library(testthat)
+library(data.table)
+library(PeakSegDisk)
+context("bad paths")
+
+bad.dir <- file.path(tempdir(), "prob (bad)")
+dir.create(bad.dir)
+foo.csv <- file.path(bad.dir, "foo.csv")
+cat("foo bar\n1 2", file=foo.csv)
+expected <- data.table(foo=1, bar=2)
+test_that("fread.first works with bad path", {
+  computed <- fread.first(foo.csv)
+  expect_equal(computed, expected)
+})
+test_that("fread.last works with bad path", {
+  computed <- fread.last(foo.csv)
+  expect_equal(computed, expected)
+})
